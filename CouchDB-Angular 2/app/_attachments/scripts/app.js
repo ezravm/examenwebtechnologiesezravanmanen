35dd81f660e69e049ea58f1a7e6eb4d2d5f14e99
ezravm <33087661+ezravm@users.noreply.github.com>
2017-11-08 17:21:05 +0100

    Final Commit
    Gave my best shot at creating a view, but did not do enough research on that.
    Omitted unnecessary service and removed some garbled code. Removed actorString in favor of actorArray.
    
    Added final screenshot
    
    added some comments here and there
    
    added a non function view.

--- CouchDB-Angular 2/app/_attachments/scripts/app.js
+++ CouchDB-Angular 2/app/_attachments/scripts/app.js
@@ -20,12 +20,10 @@ angular.module('movieApp', ['ngRoute'])
 	    		var actor = $('#actorText').val();
 	    		
 	    		actorSrv.getActor(actor).then(function(data){
-	    			var actorObject = (data.data[0].filmography.actor); //krijg juiste objectdeel terug
+	    			var actorObject = (data.data[0].filmography.actor); //krijgt juiste objectdeel terug YAY
 	    			
-	    			var actorString = "";
 	    			var actorArray = [];
 	    			for(var i = 0; i < actorObject.length; i++){
-		    			actorString += (actorObject[i].title) + " ; ";
 		    			actorArray.push(actorObject[i].title);
 		    			}
 	    			console.log(actorArray);
@@ -34,7 +32,7 @@ angular.module('movieApp', ['ngRoute'])
 	    			//saveSrv.setObject(actor ,actorArray); dit werkt niet, niet duidelijk waarom niet
 	    			
 	    			var doc = {};
-	    			doc.movies = actorString;
+	    			doc.movies = actorArray;
 	    			var json = JSON.stringify(doc)
 	    			console.log(json)
 
@@ -62,69 +60,6 @@ angular.module('movieApp', ['ngRoute'])
 	    		};
     })
     
-    /*.service('zoneSrv', function($http, $q) {
-    		this.getZones = function() {
-			var q = $q.defer();
-			$http.get('http://datasets.antwerpen.be/v4/gis/paparkeertariefzones.json')
-				.then(function(data, status, headers, config){
-					q.resolve(data.data);
-				}, function error(err) {
-					q.reject(err);
-				});
-			
-			return q.promise;
-		};
-		
-		// http://alienryderflex.com/polygon/
-		this.inPolygon = function(location, polyLoc){
-			var lastPoint = polyLoc[polyLoc.length-1];
-			var isInside = false;
-			var x = location[0];
-
-			for(var i = 0; i < polyLoc.length; i++){
-				var point = polyLoc[i];
-				var x1 = lastPoint[0];
-				var x2 = point[0];
-				var dx = x2 - x1;
-
-				if(Math.abs(dx) > 180.0){
-					if(x > 0){
-						while(x1 < 0)
-							x1 += 360;
-						while(x2 < 0)
-							x2 += 360;
-					}
-					else{
-						while(x1 > 0)
-							x1 -= 360;
-						while(x2 > 0)
-							x2 -= 360;
-					}
-					dx = x2 - x1;
-				}
-
-				if((x1 <= x && x2 > x) || (x1 >= x && x2 < x)){
-					var grad = (point[1] - lastPoint[1]) / dx;
-					var intersectAtLat = lastPoint[1] + ((x - x1) * grad);
-
-					if(intersectAtLat > location[1])
-						isInside = !isInside;
-				}
-				lastPoint = point;
-			}
-			return isInside;
-		};
-		
-		this.getTariff = function(lng, lat, zones){
-			for(var i = 0; i < zones.length; i++) {
-				var geo = JSON.parse(zones[i].geometry);
-				var coordinates = geo.coordinates[0];
-				if(this.inPolygon([lng, lat], coordinates)) {
-					return zones[i].tariefkleur;
-				}
-			}
-		};
-    })*/
     
     .service('saveSrv', function($window, $http){
 		  this.setObject = function(json, actor){
@@ -150,5 +85,7 @@ angular.module('movieApp', ['ngRoute'])
 		  
 		  this.getObject = function(key){
 			  return JSON.parse($window.localStorage[key] || '{}');
+			  //no info on how to get from Couchdb, and didn't googlefu it at home. so no spaGETti.
+			  
 		  };
 	});
---
+++ CouchDB-Angular 2/app/views/byActor/map.js
@@ -0,0 +1,5 @@
+function(doc) {
+
+	  emit(doc.movies.toLowerCase(), doc);
+  
+};
